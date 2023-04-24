da90032407dfa75b88ab9318cb18d93bf2f4b5a2
Gakles <87351405+Gakles@users.noreply.github.com>
2023-04-24 11:29:14 -0700

    created html

--- app.py
+++ app.py
@@ -13,5 +13,23 @@ app.secret_key = "Change Me"
 def setup():
     init_db()
 
+@app.route("/")
+def home():
+    return render_template("base.html")
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        #TODO: do dbsession to authenticate
+        return redirect(url_for("home"))
+
+@app.route("/addorg")
+def addorg():
+    admin = 
+    return render_template("vets.html", admin = admin)
 if __name__ == "__main__":
     app.run(debug=True)
